9f697d807b691f3c36f587596e99f04fd13db805
Jürgen Sunkler <juergen.sunkler@gmail.com>
2023-12-05 20:43:39 +0100

    feat(day2): solve day2

--- solvers/day2.go
+++ solvers/day2.go
@@ -1,24 +1,198 @@
 package solvers
 
 import (
+	"bufio"
 	"bytes"
 	"io"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-func solveDay2(input io.ReadCloser) (string, string) {
+func solveDay2(input io.ReadCloser) (string, string, error) {
 	defer input.Close()
 
 	var buf bytes.Buffer
 
 	tee := io.TeeReader(input, &buf)
 
-	return day2part1(tee), day2part2(&buf)
+	part1, err := day2part1(tee)
+	if err != nil {
+		return "", "", err
+	}
+
+	part2, err := day2part2(&buf)
+	if err != nil {
+		return "", "", err
+	}
+
+	return part1, part2, nil
 }
 
-func day2part1(input io.Reader) string {
-	return ""
+func day2part1(input io.Reader) (string, error) {
+	scanner := bufio.NewScanner(input)
+
+	regexGameId := regexp.MustCompile(`^Game (?P<gameid>\d+): (?P<draws>.*)$`)
+	regexDraw := regexp.MustCompile(`(?P<num>\d+) (?P<type>red|green|blue)`)
+
+	games := []Day2Game{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		match := regexGameId.FindStringSubmatch(line)
+
+		gameId, err := strconv.Atoi(match[regexGameId.SubexpIndex("gameid")])
+		if err != nil {
+			return "", err
+		}
+
+		currentGame := Day2Game{
+			id:    gameId,
+			draws: []Day2Draw{},
+		}
+
+		draws := strings.Split(match[regexGameId.SubexpIndex("draws")], ";")
+
+		for _, v := range draws {
+			currentDraw := Day2Draw{
+				reds:   0,
+				greens: 0,
+				blues:  0,
+			}
+
+			typeDraws := regexDraw.FindAllStringSubmatch(v, -1)
+			for _, v1 := range typeDraws {
+				val, err := strconv.Atoi(v1[regexDraw.SubexpIndex("num")])
+				if err != nil {
+					return "", err
+				}
+				switch v1[regexDraw.SubexpIndex("type")] {
+				case "red":
+					currentDraw.reds = val
+				case "green":
+					currentDraw.greens = val
+				case "blue":
+					currentDraw.blues = val
+				}
+			}
+
+			currentGame.draws = append(currentGame.draws, currentDraw)
+		}
+
+		games = append(games, currentGame)
+	}
+
+	var sum int
+
+	for _, g := range games {
+		if g.IsPossible() {
+			sum += g.id
+		}
+	}
+
+	return strconv.Itoa(sum), nil
 }
 
-func day2part2(input io.Reader) string {
-	return ""
+func day2part2(input io.Reader) (string, error) {
+	scanner := bufio.NewScanner(input)
+
+	regexGameId := regexp.MustCompile(`^Game (?P<gameid>\d+): (?P<draws>.*)$`)
+	regexDraw := regexp.MustCompile(`(?P<num>\d+) (?P<type>red|green|blue)`)
+
+	games := []Day2Game{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		match := regexGameId.FindStringSubmatch(line)
+
+		gameId, err := strconv.Atoi(match[regexGameId.SubexpIndex("gameid")])
+		if err != nil {
+			return "", err
+		}
+
+		currentGame := Day2Game{
+			id:    gameId,
+			draws: []Day2Draw{},
+		}
+
+		draws := strings.Split(match[regexGameId.SubexpIndex("draws")], ";")
+
+		for _, v := range draws {
+			currentDraw := Day2Draw{
+				reds:   0,
+				greens: 0,
+				blues:  0,
+			}
+
+			typeDraws := regexDraw.FindAllStringSubmatch(v, -1)
+			for _, v1 := range typeDraws {
+				val, err := strconv.Atoi(v1[regexDraw.SubexpIndex("num")])
+				if err != nil {
+					return "", err
+				}
+				switch v1[regexDraw.SubexpIndex("type")] {
+				case "red":
+					currentDraw.reds = val
+				case "green":
+					currentDraw.greens = val
+				case "blue":
+					currentDraw.blues = val
+				}
+			}
+
+			currentGame.draws = append(currentGame.draws, currentDraw)
+		}
+
+		games = append(games, currentGame)
+	}
+
+	var sum int
+
+	for _, g := range games {
+		maxDraw := g.Max()
+		sum += maxDraw.blues * maxDraw.greens * maxDraw.reds
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+type Day2Draw struct {
+	reds   int
+	greens int
+	blues  int
+}
+
+type Day2Game struct {
+	id    int
+	draws []Day2Draw
+}
+
+func (g *Day2Game) Max() Day2Draw {
+	maxDraw := Day2Draw{}
+
+	for _, v := range g.draws {
+		if v.reds > maxDraw.reds {
+			maxDraw.reds = v.reds
+		}
+		if v.blues > maxDraw.blues {
+			maxDraw.blues = v.blues
+		}
+		if v.greens > maxDraw.greens {
+			maxDraw.greens = v.greens
+		}
+	}
+
+	return maxDraw
+}
+
+func (g *Day2Game) IsPossible() bool {
+	maxDraw := g.Max()
+
+	if maxDraw.reds > 12 || maxDraw.greens > 13 || maxDraw.blues > 14 {
+		return false
+	}
+
+	return true
 }
--- solvers/day2_test.go
+++ solvers/day2_test.go
@@ -12,7 +12,11 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
-const testdata_day2_part2 = ``
+const testdata_day2_part2 = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
 func Test_day2part1(t *testing.T) {
 	type args struct {
@@ -27,8 +31,8 @@ func Test_day2part1(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := day2part1(tt.args.input); got != tt.want {
-				t.Errorf("day2part1() = %v, want %v", got, tt.want)
+			if got, err := day2part1(tt.args.input); got != tt.want || err != nil {
+				t.Errorf("day2part1() = %v, want %v, err %v", got, tt.want, err)
 			}
 		})
 	}
@@ -43,12 +47,12 @@ func Test_day2part2(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "testingdata day 2 part 2", args: args{input: strings.NewReader(testdata_day2_part2)}, want: "-1"},
+		{name: "testingdata day 2 part 2", args: args{input: strings.NewReader(testdata_day2_part2)}, want: "2286"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := day2part2(tt.args.input); got != tt.want {
-				t.Errorf("day2part2() = %v, want %v", got, tt.want)
+			if got, err := day2part2(tt.args.input); got != tt.want || err != nil {
+				t.Errorf("day2part2() = %v, want %v, err %v", got, tt.want, err)
 			}
 		})
 	}
