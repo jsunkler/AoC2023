5b95e6257a167d643d3091b5af4dfc49e8691b12
Jürgen Sunkler <juergen.sunkler@gmail.com>
2023-12-22 22:36:52 +0100

    feat(profiling): added feature

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/jsunkler/AoC2023/solvers"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
 	var dayFlag = flag.Int("day", 1, "Enter the day of december to process.")
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	p1, p2, err := solvers.Solve(*dayFlag)
 	if err != nil {
@@ -18,4 +36,16 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("Solved!\nPart 1\n%s\n\nPart 2\n%s\n\n", p1, p2)
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
