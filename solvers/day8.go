6de49488eb3e9150f84102c657527a0c7594617d
Jürgen Sunkler <juergen.sunkler@gmail.com>
2023-12-24 21:52:05 +0100

    feat(day8): finished

--- solvers/day8.go
+++ solvers/day8.go
@@ -5,7 +5,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -40,12 +42,60 @@ func solveDay8(input io.ReadCloser) (string, string, error) {
 func day8part1(input io.Reader) (string, error) {
 	scanner := bufio.NewScanner(input)
 
+	var commands []rune
+
+	sdMap := make(map[string]*Day8Destinations, 0)
+
+	regex := regexp.MustCompile(`^(?P<source>[A-Z]{3}) = \((?P<destleft>[A-Z]{3}), (?P<destright>[A-Z]{3})\)$`)
+
 	for row := 0; scanner.Scan(); row++ {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		if row == 0 {
+			commands = []rune(line)
+			continue
+		}
+
+		if row == 1 {
+			continue
+		}
+
+		match := regex.FindStringSubmatch(line)
+
+		source := match[regex.SubexpIndex("source")]
+		destleft := match[regex.SubexpIndex("destleft")]
+		destright := match[regex.SubexpIndex("destright")]
+
+		sdMap[source] = &Day8Destinations{
+			left:  destleft,
+			right: destright,
+		}
+
 	}
 
 	sum := 0
+	commandIndex := 0
+	currentPos := "AAA"
+
+	for {
+		if commandIndex >= len(commands) {
+			commandIndex = 0
+		}
+
+		if commands[commandIndex] == 'L' {
+			currentPos = sdMap[currentPos].left
+		}
+		if commands[commandIndex] == 'R' {
+			currentPos = sdMap[currentPos].right
+		}
+
+		commandIndex++
+		sum++
+
+		if currentPos == "ZZZ" {
+			break
+		}
+	}
 
 	return strconv.Itoa(sum), nil
 }
@@ -53,12 +103,93 @@ func day8part1(input io.Reader) (string, error) {
 func day8part2(input io.Reader) (string, error) {
 	scanner := bufio.NewScanner(input)
 
+	var commands []rune
+
+	sdMap := make(map[string]*Day8Destinations, 0)
+
+	regex := regexp.MustCompile(`^(?P<source>[1-9A-Z]{3}) = \((?P<destleft>[1-9A-Z]{3}), (?P<destright>[1-9A-Z]{3})\)$`)
+
 	for row := 0; scanner.Scan(); row++ {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		if row == 0 {
+			commands = []rune(line)
+			continue
+		}
+
+		if row == 1 {
+			continue
+		}
+
+		match := regex.FindStringSubmatch(line)
+
+		source := match[regex.SubexpIndex("source")]
+		destleft := match[regex.SubexpIndex("destleft")]
+		destright := match[regex.SubexpIndex("destright")]
+
+		sdMap[source] = &Day8Destinations{
+			left:  destleft,
+			right: destright,
+		}
+
 	}
 
-	sum := 0
+	currentPositions := make([]string, 0)
 
-	return strconv.Itoa(sum), nil
+	for k := range sdMap {
+		if strings.HasSuffix(k, "A") {
+			currentPositions = append(currentPositions, k)
+		}
+	}
+
+	sums := make([]int, 0, len(currentPositions))
+
+	for _, currentPos := range currentPositions {
+		commandIndex := 0
+		sum := 0
+
+		for {
+			if commandIndex >= len(commands) {
+				commandIndex = 0
+			}
+
+			if commands[commandIndex] == 'L' {
+				currentPos = sdMap[currentPos].left
+			}
+			if commands[commandIndex] == 'R' {
+				currentPos = sdMap[currentPos].right
+			}
+
+			commandIndex++
+			sum++
+
+			if strings.HasSuffix(currentPos, "Z") {
+				sums = append(sums, sum)
+				break
+			}
+		}
+	}
+
+	val := sums[0]
+	for i := 1; i < len(sums); i++ {
+		val = lcm(val, sums[i])
+	}
+
+	return strconv.Itoa(val), nil
+}
+
+type Day8Destinations struct {
+	left  string
+	right string
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
 }
--- solvers/day8_test.go
+++ solvers/day8_test.go
@@ -6,9 +6,32 @@ import (
 	"testing"
 )
 
-const testdata_day8_part1 = ``
+const testdata_day8_part1_1 = `RL
 
-const testdata_day8_part2 = ``
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`
+
+const testdata_day8_part1_2 = `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`
+
+const testdata_day8_part2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
 
 func Test_day8part1(t *testing.T) {
 	type args struct {
@@ -19,7 +42,8 @@ func Test_day8part1(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "testingdata day8 part 1", args: args{input: strings.NewReader(testdata_day8_part1)}, want: "-1"},
+		{name: "testingdata day8 part 1 (1)", args: args{input: strings.NewReader(testdata_day8_part1_1)}, want: "2"},
+		{name: "testingdata day8 part 1 (2)", args: args{input: strings.NewReader(testdata_day8_part1_2)}, want: "6"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -39,7 +63,7 @@ func Test_day8part2(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "testingdata day8 part 2", args: args{input: strings.NewReader(testdata_day8_part2)}, want: "-1"},
+		{name: "testingdata day8 part 2", args: args{input: strings.NewReader(testdata_day8_part2)}, want: "6"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
