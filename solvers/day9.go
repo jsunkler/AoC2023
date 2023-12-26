4828f300618117354f048ec368ac55c69e126b4a
Jürgen Sunkler <juergen.sunkler@gmail.com>
2023-12-26 21:43:33 +0100

    feat(day9): finished

--- solvers/day9.go
+++ solvers/day9.go
@@ -5,7 +5,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"slices"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -40,25 +42,119 @@ func solveDay9(input io.ReadCloser) (string, string, error) {
 func day9part1(input io.Reader) (string, error) {
 	scanner := bufio.NewScanner(input)
 
+	histories := make([]Day9History, 0)
+
 	for row := 0; scanner.Scan(); row++ {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		nums := strings.Fields(line)
+		numsInt := make([]int, 0, len(nums))
+
+		for _, num := range nums {
+			numInt, err := strconv.Atoi(num)
+			if err != nil {
+				return "", err
+			}
+			numsInt = append(numsInt, numInt)
+		}
+
+		histories = append(histories, Day9History(numsInt))
 	}
 
 	sum := 0
 
+	for _, h := range histories {
+		ext := h.extrapolate()
+		sum += ext[len(ext)-1]
+	}
+
 	return strconv.Itoa(sum), nil
+}
+
+type Day9History []int
+
+func (history Day9History) differentiate() Day9History {
+	slice := []int(history)
+
+	resultSlice := make([]int, 0, len(slice)-1)
+
+	for i := 0; i < len(slice)-1; i++ {
+		resultSlice = append(resultSlice, slice[i+1]-slice[i])
+	}
+
+	return Day9History(resultSlice)
+}
+
+func (history Day9History) isZeroed() bool {
+	slice := []int(history)
+
+	retval := true
+
+	for _, v := range slice {
+		if v != 0 {
+			retval = false
+			break
+		}
+	}
+
+	return retval
+}
+
+func (history Day9History) extrapolate() Day9History {
+	if history.isZeroed() {
+		return history.appendZero()
+	}
+
+	ext := history.differentiate().extrapolate()
+
+	slice := []int(history)
+
+	lastAdd := ext[len(ext)-1]
+
+	slice = append(slice, slice[len(slice)-1]+lastAdd)
+
+	return Day9History(slice)
+}
+
+func (history Day9History) appendZero() Day9History {
+	slice := []int(history)
+
+	slice = append(slice, 0)
+
+	return Day9History(slice)
 }
 
 func day9part2(input io.Reader) (string, error) {
 	scanner := bufio.NewScanner(input)
 
+	histories := make([]Day9History, 0)
+
 	for row := 0; scanner.Scan(); row++ {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		nums := strings.Fields(line)
+
+		slices.Reverse(nums)
+
+		numsInt := make([]int, 0, len(nums))
+
+		for _, num := range nums {
+			numInt, err := strconv.Atoi(num)
+			if err != nil {
+				return "", err
+			}
+			numsInt = append(numsInt, numInt)
+		}
+
+		histories = append(histories, Day9History(numsInt))
 	}
 
 	sum := 0
+
+	for _, h := range histories {
+		ext := h.extrapolate()
+		sum += ext[len(ext)-1]
+	}
 
 	return strconv.Itoa(sum), nil
 }
--- solvers/day9_test.go
+++ solvers/day9_test.go
@@ -6,9 +6,13 @@ import (
 	"testing"
 )
 
-const testdata_day9_part1 = ``
+const testdata_day9_part1 = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`
 
-const testdata_day9_part2 = ``
+const testdata_day9_part2 = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`
 
 func Test_day9part1(t *testing.T) {
 	type args struct {
@@ -19,7 +23,7 @@ func Test_day9part1(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "testingdata day9 part 1", args: args{input: strings.NewReader(testdata_day9_part1)}, want: "-1"},
+		{name: "testingdata day9 part 1", args: args{input: strings.NewReader(testdata_day9_part1)}, want: "114"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -39,7 +43,7 @@ func Test_day9part2(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "testingdata day9 part 2", args: args{input: strings.NewReader(testdata_day9_part2)}, want: "-1"},
+		{name: "testingdata day9 part 2", args: args{input: strings.NewReader(testdata_day9_part2)}, want: "2"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
